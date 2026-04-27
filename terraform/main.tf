8d49da5fc431934b8b9df14f81430b0dcdd57724
ratthatummanoon-kong <boonratyou@gmail.com>
2026-04-27 21:19:48 +0700

    feat: add random password resource for API key generation

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,21 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
   required_version = ">= 1.0"
+}
+
+resource "random_password" "api_key" {
+  length  = 32
+  special = false
+}
+
+locals {
+  api_key_value = var.api_key_value != "" ? var.api_key_value : random_password.api_key.result
 }
 
 provider "aws" {
