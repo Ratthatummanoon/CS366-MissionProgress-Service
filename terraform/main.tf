db189c5294d5532d559c12e32a7fe2d430629818
ratthatummanoon-kong <boonratyou@gmail.com>
2026-04-30 03:00:19 +0700

    feat: remove random password resource and simplify api_key_value assignment

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,21 +4,12 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
   required_version = ">= 1.0"
 }
 
-resource "random_password" "api_key" {
-  length  = 32
-  special = false
-}
-
 locals {
-  api_key_value = var.api_key_value != "" ? var.api_key_value : random_password.api_key.result
+  api_key_value = var.api_key_value
 }
 
 provider "aws" {
